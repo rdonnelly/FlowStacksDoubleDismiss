d93cd65e918f48256615ca6b358d2fa31bd1ae3d
Ryan Donnelly <ryan.donnelly@detroitlabs.com>
2023-11-02 10:19:11 -0400

    Initial Commit

---
+++ FlowStacksDoubleDismiss/FlowStacksDoubleDismissApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowStacksDoubleDismissApp.swift
+//  FlowStacksDoubleDismiss
+//
+//  Created by Ryan Donnelly on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FlowStacksDoubleDismissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
